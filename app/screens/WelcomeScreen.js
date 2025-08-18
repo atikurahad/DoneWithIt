7d75ba79d246361a7e239c3f972afce067747dc4
atikurahad <atikurahad@gmail.com>
2025-08-19 00:02:25 +0600

    user login signup page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,20 +5,25 @@ function WelcomeScreen() {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/camera.jpg")} // path টা তোমার ফাইল structure অনুযায়ী বদলাও
+      source={require("../assets/lll2.png")}
       resizeMode="cover"
     >
       <View style={styles.logoContainer}>
         <Image
           style={styles.iconStyle}
-          source={require("../assets/camera.jpg")}
+          source={require("../assets/logoP.png")}
         />
 
-        <Text>Sell what you don't need</Text>
+        {/* <Text style={styles.heading}>Click Every Moment</Text> */}
       </View>
 
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Sign In</Text>
+      </View>
+
+      <View style={styles.registerButton}>
+        <Text style={styles.loginButtonText}>Sign Up</Text>
+      </View>
     </ImageBackground>
   );
 }
@@ -29,21 +34,50 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
-    width: "100%",
-    height: 70,
-    color: "white",
-    backgroundColor: "orange",
+
+  heading: {
+    fontSize: 20,
+    marginTop: 20,
+    color: "purple",
+    backgroundColor: "white",
+    fontWeight: 600,
   },
+
+  loginButton: {
+    backgroundColor: "#4CAF50",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    textTransform: "uppercase",
+  },
+
+  // loginButton: {
+  //   width: "100%",
+  //   height: 70,
+  //   color: "white",
+  //   backgroundColor: "orange",
+  // },
   registerButton: {
-    width: "100%",
-    height: 70,
-    color: "white",
     backgroundColor: "purple",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 60,
+    marginTop: 10,
   },
   iconStyle: {
     height: 100,
     width: 100,
+    borderRadius: 24,
   },
   logoContainer: {
     position: "absolute",
