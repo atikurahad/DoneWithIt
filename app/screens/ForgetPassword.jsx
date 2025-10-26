de120a38be762b815cea33ae6d274643a300280f
atikurahad <atikurahad@gmail.com>
2025-10-26 22:46:52 +0600

    forgot password

--- app/screens/ForgetPassword.jsx
+++ app/screens/ForgetPassword.jsx
@@ -1,9 +1,20 @@
 import React from "react"
+import { View } from "react-native";
 
 
 const ForgotPassword = ()=>{
 
-
+<View>
+  <TextInput
+          style={styles.input}
+          placeholder="Enter your email or username"
+          placeholderTextColor="#aaa"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+        />
+</View>
 
 }
 
